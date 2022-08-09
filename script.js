132641a9da281b424898c72a3a94b72c7a05f463
Sloth <jkasahara206@gmail.com>
2022-08-08 23:04:57 -0700

    added classes to HTML and added scripts to js

--- script.js
+++ script.js
@@ -2,4 +2,38 @@
 var today = moment()
 $("#currentDay").text(today.format("MMMM Do, dddd"));
 
+var currentHour = moment().format("H");
 
+//function to change color of blocks as time passes
+var colorCode = function(){
+    let timeBlock = document.querySelectorAll(".row");
+console.log(timeBlock);
+console.log(currentHour);
+console.log("the funciton is running");
+                                    
+    if (timeBlock == currentHour) {
+        $(".description").addClass("present").removeClass("past future");
+      }
+    if (timeBlock < currentHour) {
+        $(".description").addClass("past").removeClass("present future");
+      }
+    if (timeBlock > currentHour) {
+        $(".description").addClass("future").removeClass("past present");
+      }
+
+}
+
+colorCode();
+
+
+// Save new event to local storage
+var button = document.querySelectorAll("#btn");
+var newEvent = document.querySelectorAll(".description")
+
+newEvent.textContent = newEvent
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+
+localStorage.setItem(".description", newEvent)
+})
